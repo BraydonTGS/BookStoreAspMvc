0fdf8099f72da1a04c84dbb104a3802f2ba8cbf4
Braydon Sutherland <braydonsutherland@gmail.com>
2022-11-22 14:08:02 -0600

    set up my connection string, created a category model, an applicationDbSet, added a Dbset for my model

---
+++ BookStoreWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BookStoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Establish the Connection with Entity Framework //
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options) 
+        { 
+
+        }
+
+        // Create a DB Set to create a table in your database //
+        public DbSet<Category> Categories { get; set; } 
+
+    }
+
+
+}
---
+++ BookStoreWeb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreWeb.Models
+{
+    public class Category
+    {
+        // Data Annotations for when we create our script for the DB and our Models //
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public int DisplayOrder { get; set; }   
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+
+    }
+}
